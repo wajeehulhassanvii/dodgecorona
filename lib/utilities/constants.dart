62aaf1d2b5277df54209b68ed1d573b0113aa3ee
Wajeeh Ul Hassan <wajeehulhassan_vii@hotmail.com>
2020-05-26 08:36:12 +1000

    some constants changed

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -6,17 +6,10 @@ const String kBaseUrl_temp1="http://127.0.0.1:8000";
 const String kBaseUrl="http://0.0.0.0:8000";
 //const String kBaseUrl="http://10.0.2.2:8000";
 //const String kBaseUrl="http://localhost:8000";
-//const String kBaseUrl="http://192.168.1.102:8000";
-//const String kBaseUrl="http://192.168.1.111:8000";
 const String kBaseUrl000="http://0.0.0.0:8000";
-//const String kBaseUrl="http://192.168.1.1:8000";
-//const String kBaseUrl="http://192.168.122exit.1:8000";
-//const String kBaseUrl="http://172.16.187.1:8000";
 
 //const String kBaseUrl="http://192.168.113.1:8000";
 const String kBaseUrl_chrome="http://localhost:8000";
-const String kBaseUrl_android_device="http://192.168.1.102:8000";
-const String kBaseUrl_temp2="http://192.168.1.102:8000";
 const String kFacebookAppId= '3691001584283764';
 const String kAccessTokenKey = 'access_token';
 const String kRefreshTokenKey = 'refresh_token';
